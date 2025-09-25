2e6761fa166385475cfc4100d486fca87853931c
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2025-09-25 14:39:16 -0300

    filler pq sou burro

--- The-Mysterious-Street-main/Assets/Assets/Pedro/InimigoDeJanela.cs
+++ The-Mysterious-Street-main/Assets/Assets/Pedro/InimigoDeJanela.cs
@@ -34,7 +34,7 @@ public class InimigoDeJanela : MonoBehaviour
         }
         if (objetoParaVerificar.activeInHierarchy)
         {
-            doorOpen = false;
+            enabled = false;
         }
         else
         {
